3e43ef9718afa808d78dc68372ba53ddb9b0edc6
PholawatIostream <52020781+PholawatIostream@users.noreply.github.com>
2019-06-25 21:50:45 +0700

    Add files via upload

---
+++ 02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name = "Pholawat  auttongkum";
+	int A1 = 69, A2 = 75 ;
+	char grade = 'A';
+	float GPA = 2.68;
+	cout << "My name is : " << name << endl;
+	cout << "My scores 1 : " << A1 << " points" << endl;
+	cout << "My scores 2 : " << A2 << " points" << endl;
+	cout << "Total : " << A1 + A2 << " points" << endl;
+	cout << "Grade : " << grade << endl;
+	cout << "GPA : " << GPA << endl;
+	return (0);
+}
